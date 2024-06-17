9b3e775c6ef09f3cb95dd12ff35b37e31869c965
DarkTalisman20 <155131696+DarkTalisman20@users.noreply.github.com>
2024-06-17 11:24:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,82 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/user.model.js');
 const Ticket = require('./models/tickets.model.js');
-const Interaction = require('./models/interactions.model.js');
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Testing Node API server");
 });
 
-// app.get('/products', async (req,res) =>{
-//     try{
-//         const products = await Product.find({});
-//         res.status(200).json(products);
-//     }catch(error){
-//         res.status(500).json({message: error.message});
-//     }
-// });
-
-// app.get('/product/:id', async (req,res)=>{
-//     try{
-//         const { id } = req.params;
-//         const prod = await Product.findById(id);
-//         res.status(200).json(prod);
-//     }catch(error){
-//         res.status(500).json({message: error.message});
-//     }
-// });
-
- app.post('/adduser',async (req,res) =>{
-     try{
-         const user = await User.create(req.body);
-         res.status(200).json(user);
-     }catch(error){
-         res.status(500).json({message: error.message});
-     }
- });
-
- app.post('/addticket',async (req,res) =>{
-    try{
-        const ticket = await Ticket.create(req.body);
-        res.status(200).json(ticket);
-    }catch(error){
-        res.status(500).json({message: error.message});
+// Add User Route
+app.post('/adduser', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
- });
-
-// app.put('/product/:id', async (req,res) =>{
-//     try{
-//         const {id} = req.params;
-//         const prod = await Product.findByIdAndUpdate(id,req.body);
-//         if(!prod){
-//             return res.status(404).json({message: "Product not found"});
-//         }
-//         const updatedprod = await Product.findById(id);
-//         res.status(200).json(updatedprod);
-//     }catch(error){
-//         res.status(500).json({message: error.message});
-//     }
-// });
-
-// app.delete('/product/:id', async (req,res) =>{
-//     try{
-//         const {id} = req.params;
-//         const prod = await Product.findByIdAndDelete(id);
-//         if(!prod){
-//             return res.status(404).json({message: "Product not found"});
-//         }
-//         res.status(200).json({message: "Product deleted successfully"});
-//     }catch(error){
-//         res.status(500).json({message: error.message});
-//     }
-// });
-
-mongoose.connect("mongodb+srv://epsilon:multitabletesting@cluster0.k6guqe3.mongodb.net/multitable?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-    console.log("MongoDB connected.");
-    app.listen(3000, () => {
-        console.log('Testing...');
-    });
-})
-.catch(() => {
-    console.log("Connection failed");
 });
+
+// Add Ticket Route
+app.post('/addticket', async (req, res) => {
+    try {
+        const { customer_email, organizationID, role, ...ticketData } = req.body;
+
+        // Find the user by email to get their ObjectId
+        const user = await User.findOne({ email: customer_email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // Create the ticket with references to the user's ObjectId
+        const ticket = await Ticket.create({
+            ...ticketData,
+            customer_email: user._id,
+            organizationID: user.organizationID,
+            role: user._id,
+        });
+
+        res.status(200).json(ticket);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Get Tickets Route
+app.get('/tickets', async (req, res) => {
+    try {
+        const tickets = await Ticket.find()
+            .populate({
+                path: 'customer_email',
+                select: 'email', // Select only the email field from the User model
+            })
+            .populate({
+                path: 'organizationID',
+                select: 'organizationID', // Select only the organizationID field from the User model
+            })
+            .populate({
+                path: 'role',
+                select: 'roles', // Select only the roles field from the User model
+            });
+
+        res.status(200).json(tickets);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Connect to MongoDB and Start Server
+mongoose.connect("mongodb+srv://epsilon:multitabletesting@cluster0.k6guqe3.mongodb.net/multitable?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        console.log("MongoDB connected.");
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch(() => {
+        console.log("Connection failed");
+    });
