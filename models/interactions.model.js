bbe2e7dbd099241b23a5c623ee716c64aa8145ca
DarkTalisman20 <155131696+DarkTalisman20@users.noreply.github.com>
2024-06-17 11:41:53 +0530

    Update interactions.model.js

--- models/interactions.model.js
+++ models/interactions.model.js
@@ -2,19 +2,22 @@ const mongoose = require('mongoose');
 
 const InteractionSchema = mongoose.Schema(
     {
+        ticketId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Ticket',
+            required: true,
+        },
+        problem_statement: {
+            type: String,
+            required: true,
+        },
         problem_class: {
             type: String,
             required: true,
         },
-
-        problem_statement:{
-            type: String,
-            required: true,
-        },
-
     },
     {
-        timestamps: false,
+        timestamps: true,
     }
 );
 
